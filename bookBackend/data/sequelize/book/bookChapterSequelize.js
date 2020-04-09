6559194fbf2c2c53425a00810e274073677e20d4
tfnco <380106450@qq.com>
2020-04-10 00:55:31 +0800

    no message

--- bookBackend/data/sequelize/book/bookChapterSequelize.js
+++ bookBackend/data/sequelize/book/bookChapterSequelize.js
@@ -4,6 +4,7 @@ var _ = require('lodash');
 var sequelize = require('../../../service/sequelizeConn.js');
 
 var BookChapter = require('../_models/book/bookChapter.js')
+var Book = require('../_models/book/book.js')
 
 exports.create = function(obj) {
     return new Promise(function(resolve, reject) {
@@ -13,6 +14,31 @@ exports.create = function(obj) {
             reject(err);
         });
     })
+
+    return new Promise(function(resolve, reject) {
+        sequelize.transaction({
+            deferrable: Sequelize.Deferrable.SET_DEFERRED
+        }, function(t) {
+            var all = []
+            all.push(BookChapter.create(obj, {
+                transaction: t
+            }))
+            all.push(Book.increment({
+                chapterCount: 1
+            }, {
+                where: {
+                    bookId: obj.bookId
+                },
+                transaction: t
+            }));
+            return Promise.all(all);
+        }).then(function(results) {
+            resolve(results[0])
+        }, reject).catch(function(err) {
+            reject(err);
+        })
+    })
+
 }
 
 exports.update = function(obj, where) {
