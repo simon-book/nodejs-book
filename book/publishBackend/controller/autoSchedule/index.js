21022e4c03867e56ea9080a8d1d6ac8b5d018311
陈猛 <380106450@qq.com>
2021-05-31 00:38:41 +0800

    自动更新

--- book/publishBackend/controller/autoSchedule/index.js
+++ book/publishBackend/controller/autoSchedule/index.js
@@ -6,21 +6,27 @@ var copyDashenController = require("../copy_dashen/copyDashenController.js");
 
 function auto_schedule() {
     //每日更新
-    schedule.scheduleJob('0 0 4 * * *', async function() {
+    schedule.scheduleJob('0 0 16 * * *', async function() {
+        if (moment().isoWeekday() == 1) return false;
+        console.log("更新biqu最近2天数据！");
         await copyBiqugeController.queryBranchInfo();
         copyBiqugeController.copy_all_books();
     });
-    schedule.scheduleJob('0 0 4 * * *', async function() {
+    schedule.scheduleJob('0 0 16 * * *', async function() {
+        if (moment().isoWeekday() == 2) return false;
+        console.log("更新dashen最近2天数据！");
         await copyDashenController.queryBranchInfo();
         copyDashenController.copy_all_books();
     });
 
     //每周全部更新
-    schedule.scheduleJob('0 0 0 * * 1', async function() {
+    schedule.scheduleJob('0 0 12 * * 1', async function() {
+        console.log("全量更新biqu数据！");
         await copyBiqugeController.queryBranchInfo();
         copyBiqugeController.copy_all_books(-1);
     });
-    schedule.scheduleJob('0 0 8 * * 1', async function() {
+    schedule.scheduleJob('0 0 12 * * 2', async function() {
+        console.log("全量更新dashen数据！");
         await copyDashenController.queryBranchInfo();
         copyDashenController.copy_all_books(-1);
     });
